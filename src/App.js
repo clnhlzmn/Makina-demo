251c3eaebe72bb635630f332db67dec606258a22
Colin Holzman <colin.holzman@protonmail.com>
2021-03-18 19:42:23 -0400

    Improved input text area and styling, added 'compile' button.

--- src/App.js
+++ src/App.js
@@ -3,34 +3,41 @@ import Tabs from "./components/Tabs"
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.setState({value: "foo"})
-    this.onChange = this.onChange.bind(this)
-  }
-  onChange(event) {
-    console.log(event.target.value)
-    this.setState({value: event.target.value})
-  }
-  render() {
-    return (
-      <div>
-        <h1>Tabs Demo</h1>
-        <Tabs> 
-          <div label="Input"> 
-            <textarea className="input" rows="80" 
-                      cols="80" id="aboutDescription"/>
-          </div> 
-          <div label="C Header"> 
-            After 'while, <em>Crocodile</em>! 
-          </div> 
-          <div label="C Implementation"> 
-            Nothing to see here, this tab is <em>extinct</em>! 
-          </div> 
-        </Tabs> 
-      </div>
-    );
-  }
+    
+    constructor(props) {
+        super(props)
+        this.onChange = this.onChange.bind(this)
+    }
+    
+    onChange(event) {
+        this.setState({input: event.target.value})
+    }
+    
+    render() {
+        console.log("App.render")
+        return (
+            <div>
+                <h1>Tabs Demo</h1>
+                <Tabs> 
+                    <div label="Input"> 
+                        <div className="input-container">
+                            <textarea className="input" rows="80" 
+                                    cols="80" id="aboutDescription" 
+                                    onChange={this.onChange}
+                                    value={this.state?.input || ""}/>
+                        </div>
+                        <button className="submit" type="button">Compile</button>
+                    </div> 
+                    <div label="C Header"> 
+                        After 'while, <em>Crocodile</em>! 
+                    </div> 
+                    <div label="C Implementation"> 
+                        Nothing to see here, this tab is <em>extinct</em>! 
+                    </div> 
+                </Tabs> 
+            </div>
+        );
+    }
 }
 
 export default App;
