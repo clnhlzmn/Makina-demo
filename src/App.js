87c2807873a88ba6d6db9284bf659f8c27402059
Colin Holzman <colin.holzman@protonmail.com>
2021-03-19 12:42:14 -0400

    Added button on click to send input text to makina-function, cross origin request blocked.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,36 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.onChange = this.onChange.bind(this)
+        this.onClickCompile = this.onClickCompile.bind(this)
     }
     
     onChange(event) {
         this.setState({input: event.target.value})
     }
     
+    onClickCompile(event) {
+        console.log(this.state?.input || "")
+        
+        // create a new XMLHttpRequest
+        var xhr = new XMLHttpRequest()
+        // get a callback when the server responds
+        xhr.addEventListener('load', () => {
+            // update the state of the component with the result here
+            console.log(xhr.responseText)
+        })
+        // open the request with the verb and the url
+        xhr.open('GET', 'http://localhost:8080')
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:8080")
+        xhr.setRequestHeader("Content-Type", "text/plain")
+        // send the request
+        xhr.send(this.state?.input || "")
+    }
+    
     render() {
         console.log("App.render")
         return (
             <div>
-                <h1>Tabs Demo</h1>
+                <h1>Makina Demo</h1>
                 <Tabs> 
                     <div label="Input"> 
                         <div className="input-container">
@@ -26,7 +45,9 @@ class App extends Component {
                                     onChange={this.onChange}
                                     value={this.state?.input || ""}/>
                         </div>
-                        <button className="submit" type="button">Compile</button>
+                        <button className="submit" 
+                                type="button"
+                                onClick={this.onClickCompile}>Compile</button>
                     </div> 
                     <div label="C Header"> 
                         After 'while, <em>Crocodile</em>! 
